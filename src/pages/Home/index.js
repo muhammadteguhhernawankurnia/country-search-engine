83c059f904cd37676555faa8ad5e29066f90d897
muhammadteguhhernawankurnia <moch.teguhhernawankurnia@gmail.com>
2023-06-04 07:43:34 +0700

    slicing done, search done, get api at result page still static, from result country still not link with result

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,75 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
 import glassSearch from '../.././assets/icons/glass-search.svg';
 
 const HomePage = () => {
+  const [countries, setCountries] = useState([]);
+  const [searchInput, setSearchInput] = useState('');
+  const [suggestedCountries, setSuggestedCountries] = useState([]);
+  const [showNotFound, setShowNotFound] = useState(false);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      const result = await axios('https://restcountries.com/v3.1/all');
+      setCountries(result.data);
+    };
+
+    fetchCountries();
+  }, []);
+
+  useEffect(() => {
+    let timeoutId = null;
+
+    if (showNotFound) {
+      timeoutId = setTimeout(() => {
+        setShowNotFound(false);
+      }, 1000);
+    }
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [showNotFound]);
+
+  const handleSearchInputChange = (e) => {
+    const inputValue = e.target.value;
+    setSearchInput(inputValue);
+    if (inputValue.length > 0) {
+      const filteredCountries = countries.filter((country) =>
+        country.name.common.toLowerCase().includes(inputValue.toLowerCase())
+      );
+      const topFiveCountries = filteredCountries.slice(0, 5);
+      setSuggestedCountries(topFiveCountries);
+      setShowNotFound(false);
+    } else {
+      setSuggestedCountries([]);
+      setShowNotFound(false);
+    }
+  };
+
+  const handleInputBlur = () => {
+    setShowNotFound(true);
+  };
+
   return (
     <>
       <div className='container'>
         <div className='grid justify-items-center items-center h-screen'>
           <div className='w-full max-w-lg'>
-            <div className='text-[72px] font-bold text-[#000000] text-center'>
-              Country
-            </div>
+            <Link to='/result'>
+              <div className='text-[72px] font-bold text-[#000000] text-center'>
+                Country
+              </div>
+            </Link>
             <div className='relative'>
               <input
                 type='text'
                 placeholder='Type any country name'
+                value={searchInput}
+                onChange={handleSearchInputChange}
+                onBlur={handleInputBlur}
                 className='input input-dark input-bordered w-full max-w-lg focus:border-[#8362F2] shadow-md'
               />
               <img
@@ -20,6 +77,26 @@ const HomePage = () => {
                 className='ml-[92%] mt-[-7%]'
                 alt='glass-search'
               />
+              {suggestedCountries.length > 0 ? (
+                <ul className='absolute left-0 right-0 mt-2 bg-white border border-gray-200 shadow-md'>
+                  {suggestedCountries.map((country) => (
+                    <li key={country.name.common}>
+                      <Link
+                        to={`/result/${encodeURIComponent(
+                          country.name.common
+                        )}`}
+                        className='block px-4 py-2 hover:bg-gray-100'
+                      >
+                        {country.name.common}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              ) : showNotFound ? (
+                <div className='absolute left-0 right-0 mt-4'>
+                  <p className='block px-4 py-2 text-red-600'>Data not found</p>
+                </div>
+              ) : null}
             </div>
           </div>
         </div>
--- src/pages/Result/index.js
+++ src/pages/Result/index.js
@@ -1,9 +1,31 @@
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import arrowLeft from '../.././assets/icons/arrow-left.svg';
 import globe from '../.././assets/icons/globe.svg';
 
 const ResultPage = () => {
+  const [countryData, setCountryData] = useState(null);
+
+  useEffect(() => {
+    const fetchCountryData = async () => {
+      try {
+        const response = await axios.get(
+          'https://restcountries.com/v3.1/name/japan'
+        );
+        // const response = await axios.get(
+        //   'https://restcountries.com/v3.1/name/${name}'
+        // );
+        const country = response.data[0];
+        setCountryData(country);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchCountryData();
+  }, []);
   return (
     <>
       <div className='container'>
@@ -14,19 +36,25 @@ const ResultPage = () => {
               <p className='font-semibold'>Back to HomePage</p>
             </button>
           </Link>
-          <div>
-            <p className='text-5xl font-bold'>Indonesia</p>
-            {/* <div className='flag'></div> */}
+          <div className='flex mb-2'>
+            <p className='text-5xl font-bold mr-4'>
+              {countryData?.name?.common}
+            </p>
+            <img
+              className='w-18 h-12 shadow-sm border'
+              src={countryData?.flags.svg}
+              alt='Flag'
+            />
           </div>
           <div className='flex p-1'>
             <div className='bg-[#8DD4CC] rounded-xl text-white text-sm mr-2 max-w-fit p-2'>
-              ID
+              {countryData?.altSpellings[0]}
             </div>
             <div className='bg-[#8DD4CC] rounded-xl text-white text-sm mr-2 max-w-fit p-2'>
-              Republic of Indonesia
+              {countryData?.altSpellings[1]}
             </div>
             <div className='bg-[#8DD4CC] rounded-xl text-white text-sm mr-2 max-w-fit p-2'>
-              Republik Indonesia
+              {countryData?.altSpellings[2]}
             </div>
           </div>
 
@@ -36,7 +64,7 @@ const ResultPage = () => {
               <div className='mr-2'>
                 <p className='text-xl font-semibold'>LatLong</p>
                 <p className='text-4xl font-bold text-[#8362F2] mr-24'>
-                  -5.0, 120.0
+                  {countryData?.latlng}
                 </p>
               </div>
               <div>
@@ -46,10 +74,14 @@ const ResultPage = () => {
 
             <div className='flex border shadow-xl rounded-lg w-[40%] h-30 p-4'>
               <div className='mr-2'>
-                <p className='text-xl font-semibold'>Capital: Jakarta</p>
-                <p className='text-xl font-semibold'>Region: Asia</p>
                 <p className='text-xl font-semibold'>
-                  Subregion: South-Eastern Asia
+                  Capital: {countryData?.capital}
+                </p>
+                <p className='text-xl font-semibold'>
+                  Region: {countryData?.region}
+                </p>
+                <p className='text-xl font-semibold'>
+                  Subregion: {countryData?.subregion}
                 </p>
               </div>
             </div>
@@ -61,14 +93,17 @@ const ResultPage = () => {
             <div className='flex w-[40%] h-30 p-4 mr-5'>
               <div className='mr-2'>
                 <p className='text-xl font-semibold'>Calling Code</p>
-                <p className='text-4xl font-bold text-[#8362F2] mr-24'>62</p>
+                <p className='text-4xl font-bold text-[#8362F2] mr-24'>
+                  {countryData?.idd?.root}
+                  {countryData?.idd?.suffixes}
+                </p>
                 <div className='flex'>
                   <p className='text-xl font-semibold mr-1 underline text-[#8362F2]'>
                     1 country
                   </p>
                   <p className='text-xl font-semibold'>
                     {' '}
-                    1 country with this calling code
+                    with this calling code
                   </p>
                 </div>
               </div>
@@ -77,7 +112,17 @@ const ResultPage = () => {
             <div className='flex w-[40%] h-30 p-4'>
               <div className='mr-2'>
                 <p className='text-xl font-semibold'>Currency</p>
-                <p className='text-4xl font-bold text-[#8362F2] mr-24'>IDR</p>
+                <p className='text-4xl font-bold text-[#8362F2] mr-24'>
+                  {countryData?.currencies && (
+                    <div>
+                      {Object.keys(countryData.currencies).map((currency) => (
+                        <div key={currency}>
+                          {countryData.currencies[currency].name}
+                        </div>
+                      ))}
+                    </div>
+                  )}
+                </p>
                 <div className='flex'>
                   <p className='text-xl font-semibold mr-1 underline text-[#8362F2]'>
                     1 country
